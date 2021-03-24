2be7794c1b30d58ca01e8aec1db6dfed8a6d027e
jopringle <whosthatnewguy@gmail.com>
2021-03-23 22:37:55 -0500

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const url = 'https://1q8e5OYcJFN3yGuZ5dGxg9cClqx:b0c76a22ffa36590716d9044c2cfad2
 const web3 = new Web3(Web3.givenProvider || url);
 
 // connect to individual eth node from https://etherscan.io/
-// var address = '0xb105608D0Fb6FEC617AfDD7D5147Aa7Ab7a86612'
+// var address = '#####'
 
 // // web3.eth allows us to interact with eth network functions
 // // callback arrow function takes error and balance
